8863da0591ea8cff6290534ac2f5b84080b8ca8d
aslihancelik <aslihancelik27@gmail.com>
2024-09-18 17:35:18 -0400

    developing

--- MathExpressionsAndInteractiveIO.cpp
+++ MathExpressionsAndInteractiveIO.cpp
@@ -1,10 +1,53 @@
 // MathExpressionsAndInteractiveIO.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    double principal, interestRate, timesCompounded, amountSavings, interestAmount;
+    cout << "Please enter the principal amount:";
+    cin >> principal;
+    cout << "Please enter what percent the interest rate is:";
+    cin >> interestRate;
+    cout << "Please enter the number of times the interest is compounded:";
+    cin >> timesCompounded;
+
+    amountSavings = principal * pow((1 + interestRate / timesCompounded), timesCompounded);
+    interestAmount = amountSavings - principal;
+
+    //interest rate turn into %
+
+    /*
+    cout << left << setw(20) << "Interest Rate: " << fixed << setprecision(2) << right << setw(10) << interestRate << "%" << endl;
+    cout << left << setw(20) << "Times Compounded: " << fixed << setprecision(0) << right << setw(10) << timesCompounded << endl;
+    cout << left << setw(20) << "Principal: " << right << setw(10) << "$" << fixed << setprecision(2)  << principal << endl;
+    cout << left << setw(20) << "Interest: " << right << setw(10) << "$" << amountSavings << endl;
+    */
+
+    // Display the formatted output
+    
+    cout << left << setw(25) << "Interest Rate:"
+        << right << setw(10) << fixed << setprecision(2) << interestRate << "%" << endl;
+
+    cout << left << setw(25) << "Times Compounded:"
+        << right << setw(10) << fixed << setprecision(0) << timesCompounded << endl;
+
+    cout << left << setw(25) << "Principal:"
+        << right << "$" << setw(10) << fixed << setprecision(2) << principal << endl;
+
+    cout << left << setw(25) << "Interest:"
+        << right << "$" << setw(10) << fixed << setprecision(2) << interestAmount << endl;
+
+    cout << left << setw(25) << "Amount in Savings:"
+        << right << "$" << setw(10) << fixed << setprecision(2) << amountSavings << endl;
+
+
 }
 
