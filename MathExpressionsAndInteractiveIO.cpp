5fb1705f80b8de30f3c50d89b1f86f3ae33a3e75
aslihancelik <aslihancelik27@gmail.com>
2024-09-18 20:18:45 -0400

    Finished the changes.

--- MathExpressionsAndInteractiveIO.cpp
+++ MathExpressionsAndInteractiveIO.cpp
@@ -1,27 +1,38 @@
-// MathExpressionsAndInteractiveIO.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer: Aslihan Celik
 //
+//
+// MathExpressionsAndInteractiveIO.cpp
+// This program calculates and displays ingredient amounts for a cookie recipe based on user input.
+// It adjusts the quantities for any number of cookies and formats the results to two decimal places.
+
 
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <sstream> // Include for std::ostringstream
+
 using namespace std;
 
 int main()
-{
+{   //constants for multipliers of each ingredient to calculate the ingredients for the number of cookies specified
     const double SUGAR_MULTIPLIER = 1.5, BUTTER_MULTIPLIER = 1, FLOUR_MULTIPLIER = 2.75;
+    
     const int NUMBER_OF_COOKIES = 48;
-    double numberOfCookies;
-    double ingredientMultiplier;
+
+    double numberOfCookies;             // Number of cookies the user wants to make
+    double ingredientMultiplier;        // Multiplier to scale ingredients based on user input
 
     cout << "Please enter the number of cookies you would like to make. \n";
     cin >> numberOfCookies;
+
+    // Calculate the ingredient multiplier based on the number of cookies
     ingredientMultiplier = numberOfCookies/ NUMBER_OF_COOKIES;
 
+    // Calculate the amount of each ingredient needed
     double sugarNeeded = SUGAR_MULTIPLIER * ingredientMultiplier;
     double butterNeeded = BUTTER_MULTIPLIER * ingredientMultiplier;
     double flourNeeded = FLOUR_MULTIPLIER * ingredientMultiplier;
 
+    // Display the amount of each ingredient needed
     cout << fixed << setprecision(2);
     cout << "For the number of cookies you entered you need the following amount of each ingredient:" << endl;
     cout << setw(10) << sugarNeeded << left << setw(20) << (sugarNeeded != 1 ? " cups of sugar" : " cup of sugar") << endl;
